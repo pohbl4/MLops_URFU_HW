614418942e581d2cdf6b708c411b34ad78ff7ae6
Ron <80053176+pohbl4@users.noreply.github.com>
2024-06-22 22:53:59 +0500

    Update get_data.py

--- HW4/scripts/get_data.py
+++ HW4/scripts/get_data.py
@@ -1,28 +1,40 @@
+# DON'T FORGET INSTALL DEPENDENCIES
+# pip install catboost PANDAS
+import pandas as pd
 import os
+from catboost.datasets import titanic
 
 
-train_path, test_path = "../titanic-dataset/train.csv", "../titanic-dataset/test.csv"
+root_path = ".."
+dataset_path = "titanic-dataset"
+train_path, test_path = "train.csv", "test.csv"
 
+# CREATE DATASET DIR IF DOES NOT EXIST
+if not os.path.exists(os.path.join(root_path, dataset_path)):
+    os.mkdir(os.path.join(root_path, dataset_path))
 
-# DELETE DATA
-def remove_file(file_path):
-    if os.path.exists(file_path):
-        os.remove(file_path)
+full_train_path = os.path.join(root_path, dataset_path, train_path)
+full_test_path = os.path.join(root_path, dataset_path, test_path)
+
+# GET DATA
+def create_and_save_dataset(file_path, number_of_dataset):
+    if not os.path.exists(file_path):
+        titanic_ds = titanic()[number_of_dataset]
+        df = pd.DataFrame(titanic_ds)
+        df.to_csv(file_path)
     else:
-        print(f"Cannot find file for removing - {file_path}")
+        print(f"File already exists - {file_path}") 
 
 
 # CHECK DATA
 def check_file(file_path):
-    if not os.path.exists(file_path):
-        print(f"Success removing - {file_path}")
+    if os.path.exists(file_path):
+        print(f"File exists - {file_path}")
     else:
-        print(f"Fail removing - {file_path}")
+        prinit(f"Fail downloading - {file_path}")
 
 
-
-remove_file(train_path)
-check_file(train_path)
-
-remove_file(test_path)
-check_file(test_path)
+create_and_save_dataset(full_train_path, 0)
+create_and_save_dataset(full_test_path, 1)
+check_file(full_train_path)
+check_file(full_test_path)
