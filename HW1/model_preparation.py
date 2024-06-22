adc2e7fac6e8f042b94085a486499b48ba91c3c5
Ron <80053176+pohbl4@users.noreply.github.com>
2024-06-22 22:27:51 +0500

    Update model_preparation.py

--- HW1/model_preparation.py
+++ HW1/model_preparation.py
@@ -1,24 +1,18 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-import os
+import pickle
 
 
 df_train = pd.read_csv('./train/result_train.csv')
-df_test = pd.read_csv('./test/result_test.csv')
+
 
 X_train = df_train.drop('target', axis=1)
 y_train = df_train['target']
-X_test = df_test.drop('target', axis=1)
 
 model = LinearRegression()
 print("--- Model created")
 
-predict_path = os.path.join("./test", "predict.csv")
 model.fit(X_train, y_train)
 print("--- Model trained")
 
-predict_df = pd.DataFrame(model.predict(X_test))
-predict_df.to_csv(predict_path, index=False)
-
-if os.path.exists(predict_path):
-    print(f"--- The model performed the calculation and wrote the data to a file {predict_path}")
+pickle.dump(model, open('model.pkl', 'wb'))
